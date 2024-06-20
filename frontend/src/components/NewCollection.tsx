7e6e5ccd15b555345311797e78595838352d1fb0
buddini96 <buddinikl1996@gmail.com>
2024-06-20 21:52:31 +0530

    content updated

--- frontend/src/components/NewCollection.tsx
+++ frontend/src/components/NewCollection.tsx
@@ -45,7 +45,7 @@ const NewCollection: React.FC = () => {
 
   return (
     <section className="bg-primary">
-      <div className="max_padd_container py-12 xl:py-28 xl:w-[97%]">
+      <div className="max_padd_container py-12 xl:py-28 xl:w-[99%]">
         <h3 className="h3 text-center">Latest Products</h3>
         <hr className="h-[3px] md:w-1/2 mx-auto bg-gradient-to-1 from-transparent via-black to-transparent mb-16" />
         <div className="grid grid-cols-1 xs:grid-cols-2 md:grid-cols-3 xl:grid-cols-4 gap-6">
